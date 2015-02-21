92416acf261e7260d07d57a99e55f7b13da7306e
Cyrill Schumacher <cyrill@schumacher.fm>
2015-02-22 09:54:20 +1100

    Disable loading of terrible implemented categories
    @todo import taxonomies into a database table and then use instead of
    a select a grid system.
    Modman file has new paths.

--- app/code/community/BlueVisionTec/GoogleShoppingApi/Model/Attribute/Source/GoogleShoppingCategories.php
+++ app/code/community/BlueVisionTec/GoogleShoppingApi/Model/Attribute/Source/GoogleShoppingCategories.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @category	BlueVisionTec
- * @package    BlueVisionTec_GoogleShoppingApi
+ * @category    BlueVisionTec
+ * @package     BlueVisionTec_GoogleShoppingApi
  * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
  * @copyright   Copyright (c) 2015 BlueVisionTec UG (haftungsbeschränkt) (http://www.bluevisiontec.de)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -10,42 +10,57 @@
 /**
  * Adminhtml GoogleShopping Store Switcher
  *
- * @category	BlueVisionTec
- * @package    BlueVisionTec_GoogleShoppingApi
+ * @category    BlueVisionTec
+ * @package     BlueVisionTec_GoogleShoppingApi
  * @author      Magento Core Team <core@magentocommerce.com>
  * @author      BlueVisionTec UG (haftungsbeschränkt) <magedev@bluevisiontec.eu>
  */
-class BlueVisionTec_GoogleShoppingApi_Model_Attribute_Source_GoogleShoppingCategories extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
+class BlueVisionTec_GoogleShoppingApi_Model_Attribute_Source_GoogleShoppingCategories
+    extends Mage_Eav_Model_Entity_Attribute_Source_Abstract
 {
-    const TAXONOMY_FILE_PATH = "/var/bluevisiontec/googleshoppingapi/data/";
-
     /**
+     * @todo move that into DB tables!
+     * @todo implement it via AJAX because this slows down the load of catalog/product/edit also terrible implemented
+     * :-(
+     * implement as a grid system once data is in a table. this also allows multi category selects (if supported by
+     * google)
+     *
      * Retrieve all options array
      *
      * @return array
      */
     public function getAllOptions()
     {
-        $taxonomyPath = Mage::getBaseDir() . self::TAXONOMY_FILE_PATH;
+        $taxonomyPath = Mage::getModuleDir('', 'BlueVisionTec_GoogleShoppingApi') . DS . 'taxonomies' . DS;
+        $lang         = Mage::getStoreConfig('general/locale/code', Mage::app()->getRequest()->getParam('store', 0));
+        $taxonomyFile = $taxonomyPath . $lang . '.txt';
 
-        $lang = Mage::getStoreConfig('general/locale/code',Mage::app()->getRequest()->getParam('store', 0));
-        $taxonomyFile = $taxonomyPath . "taxonomy.".$lang.".txt";
-        
-        if(!file_exists($taxonomyFile)) {
-			$taxonomyFile = $taxonomyPath . "taxonomy.en_US.txt";
+        if (false === file_exists($taxonomyFile)) {
+            $taxonomyFile = $taxonomyPath . 'en_US.txt';
         }
-        
-        if (is_null($this->_options)) {
-            if(($fh = fopen($taxonomyFile,"r")) !== false) {
-                $line = 0;
-                $this->_options = array();
+
+        if (null === $this->_options) {
+            $this->_options = [
+                [
+                    'value' => 0,
+                    'label' => 'Categories not yet implemented'
+                ]
+            ]; // @todo this is a hack to avoid loading
+            return $this->_options;
+
+            if (($fh = fopen($taxonomyFile, 'r')) !== false) {
+                $line           = 0;
+                $this->_options = [];
                 while (($category = fgets($fh)) !== false) {
-                    if($line === 0) {$line++;continue;} // skip first line
+                    if ($line === 0) {
+                        $line++;
+                        continue;
+                    } // skip first line
                     $line++;
-                    $this->_options[] = array(
+                    $this->_options[] = [
                         'value' => $line,
-                        'label' => $line ." ". $category
-                    );
+                        'label' => $line . ' ' . $category
+                    ];
                 }
             }
         }
